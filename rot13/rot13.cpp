b9e8a99adab814b5f0d14d45099a3d910166b9bc
David Aizenberg <david.aizenberg@paranoici.org>
2014-07-08 21:30:43 +0100

    error

--- rot13/rot13.cpp
+++ rot13/rot13.cpp
@@ -9,7 +9,14 @@ int main() {
         getline(cin, phrase);
         int length;
         for(length = 0;; length++) if(!phrase[length]) break;
+        int rot[length];
 
+        for (int i = 0; i < length; i++) {
+               rot[i] = (int) phrase[i] + 13;
+                if(rot13[i] > 122) rot13[i] - 25; 
+               cout << (char) rot[i];
+        }
+        cout << endl;
         for (int i = 0; i < length; i++) {
                cout << phrase[i];
                cout << "  ";
