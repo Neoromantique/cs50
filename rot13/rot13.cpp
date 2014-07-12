6d38e6f4796b5c17d32ab9d403c84ac5c1eaae8a
David Aizenberg <david.aizenberg@paranoici.org>
2014-07-12 02:48:06 +0100

    lil'

--- rot13/rot13.cpp
+++ rot13/rot13.cpp
@@ -4,36 +4,25 @@
 #define LETTERS 26
 #define LASTCHARACTER 122
 
-
 using namespace std;
 
 int main() {
 
         string phrase = "";
-        
         cout << "Enter phrase to encrypt or decrypt: ";
-        
         getline(cin, phrase);
         size_t length = phrase.size();
-       //for(length = 0;; length++) if(!phrase[length]) break;
-       // int rot[length];
-        
         cout << "Encrypted/Decrypted Phrase: ";
-
         for (int i = 0; i < length; i++) {
                char c = phrase[i] + 13;
-               
                if(isupper(c)) {
-                        //if(rot[i] > LASTCHARACTER - 33) { rot[i] = rot[i] - LETTERS; }
                         if(c > LASTCHARACTER) { c = c - LETTERS; }
-
                }
                else if(islower(c)) {
                         if(c >= LASTCHARACTER) { c = c - LETTERS; }
                }
                cout << c;
         }
- 
         cout << endl;
 }
 
