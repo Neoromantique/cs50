66e11d01048492e5d42a28639b4dd2306e1d1923
David Aizenberg <david.aizenberg@paranoici.org>
2014-07-12 04:03:39 +0100

    It works, at last.

--- rot13/rot13.cpp
+++ rot13/rot13.cpp
@@ -14,16 +14,18 @@ int main() {
         size_t length = phrase.size();
         cout << "Encrypted/Decrypted Phrase: ";
         for (int i = 0; i < length; i++) {
-               char c = phrase[i];
-               if(isupper(c)) {
-                        if(c + 13  > LASTCHARACTER - 33) { c = c + 13 - LETTERS; }
+               int c = phrase[i];
+               if(isupper(c)) { 
+                       c+=13;
+                       if(c > LASTCHARACTER - 33) { c = c - LETTERS; }
 //                        cout << " upper ";
 }
                else if(islower(c)) {
-                        if(c + 13 >= LASTCHARACTER) { c = c + 13 - LETTERS; }
+                       c+=13;
+                        if(c > LASTCHARACTER) { c = c - LETTERS; }
 //                        cout << " lower ";
 }
-               cout << c;
+               cout << (char) c;
         }
         cout << endl;
 }
